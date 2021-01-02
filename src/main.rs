59d12f6ea7d74a48a69a65fe671904f5515c2081
David James McCorrie <djmccorrie@gmail.com>
2021-01-02 00:16:28 +0000

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use jsonrpc_http_server::{DomainsValidation, AccessControlAllowOrigin, ServerBuilder};
-use jsonrpc_http_server::jsonrpc_core::{IoHandler};
+use jsonrpc_http_server::jsonrpc_core::IoHandler;
+use jsonrpc_http_server::{AccessControlAllowOrigin, DomainsValidation, ServerBuilder};
 
 mod methods;
 
 fn main() {
-	  let mut io = IoHandler::default();
-	  io.add_method("say_hello", methods::say_hello);
+    let mut io = IoHandler::default();
+    io.add_method("say_hello", methods::say_hello);
     io.add_method("say_bye", methods::say_bye);
 
-	  let server = ServerBuilder::new(io)
-		    .cors(DomainsValidation::AllowOnly(vec![AccessControlAllowOrigin::Null]))
-		    .start_http(&"127.0.0.1:3030".parse().unwrap())
-		    .expect("Unable to start RPC server");
+    let server = ServerBuilder::new(io)
+        .cors(DomainsValidation::AllowOnly(vec![
+            AccessControlAllowOrigin::Null,
+        ]))
+        .start_http(&"127.0.0.1:3030".parse().unwrap())
+        .expect("Unable to start RPC server");
 
-	  server.wait();
+    server.wait();
 }
